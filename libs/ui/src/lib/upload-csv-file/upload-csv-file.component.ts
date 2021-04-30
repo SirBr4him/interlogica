7ac5b5148c579a41825b3d676a98fa75f1166de7
Brahim Benlarbi <brahim.benlarbi@wintech.it>
2021-04-30 18:37:51 +0200

    created upload csv file component

---
+++ libs/ui/src/lib/upload-csv-file/upload-csv-file.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'interlogica-upload-csv-file',
+  templateUrl: './upload-csv-file.component.html',
+  styleUrls: ['./upload-csv-file.component.scss']
+})
+export class UploadCsvFileComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
